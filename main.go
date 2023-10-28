97e1d0f037642089cc71ba5015136fde3b8e70f1
moyangmu <729227315@qq.com>
2023-10-28 23:23:11 +0800

    hjh 修改项目架构3

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"Projectdouy/route"
-	"gorm.io/driver/mysql"
-
 	"gorm.io/gorm"
 )
 
@@ -12,23 +10,6 @@ import (
 var Db *gorm.DB
 
 func main() {
-	dsn := "root:123456@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local"
-
-	_db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("连接数据库失败, error=" + err.Error())
-	}
-
-	if err != nil {
-		panic("连接数据库失败, error=" + err.Error())
-	}
-	Db = _db
-	sqlDB, _ := Db.DB()
-
-	//设置数据库连接池参数
-	sqlDB.SetMaxOpenConns(100) //设置数据库连接池最大连接数
-	sqlDB.SetMaxIdleConns(20)  //连接池最大允许的空闲连接数，如果没有sql任务需要执行的连接数大于20，超过的连接会被连接池关闭。
-
 	//初始化操作
 	r := routes.SetRouter()
 
--- repository/models.go
+++ repository/models.go
@@ -1,16 +1,34 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 type User struct {
-	gorm.Model
-	Username string `gorm:"unique"`
-	Password string
+	ID            int64     `gorm:"column:id;AUTO_INCREMENT;primary_key" json:"id"`
+	Username      string    `gorm:"column:username;NOT NULL" json:"username"`
+	Password      string    `gorm:"column:password;NOT NULL" json:"password"`
+	Gender        int       `gorm:"column:gender;default:0;NOT NULL" json:"gender"`                                                                                                                                                                                           // 0 男 1女
+	FollowerCount int       `gorm:"column:follower_count;default:0;NOT NULL" json:"follower_count"`                                                                                                                                                                           // 粉丝数
+	FollowCount   int       `gorm:"column:follow_count;default:0;NOT NULL" json:"follow_count"`                                                                                                                                                                               // 关注数
+	Face          string    `gorm:"column:face;default:https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg/800px-Everest_North_Face_toward_Base_Camp_Tibet_Luca_Galuzzi_2006.jpg;NOT NULL" json:"face"` // 头像地址
+	CreateTime    time.Time `gorm:"column:create_time;default:CURRENT_TIMESTAMP;NOT NULL" json:"create_time"`
+	UpdateTime    time.Time `gorm:"column:update_time;default:CURRENT_TIMESTAMP;NOT NULL" json:"update_time"`
+	IsDeleted     int       `gorm:"column:is_deleted;default:0;NOT NULL" json:"is_deleted"` // (0-, 1-)
 }
 
+// `video`
 type Video struct {
-	gorm.Model
-	UserID uint
-	Title  string
-	URL    string
+	ID            int64     `gorm:"column:id;AUTO_INCREMENT;primary_key" json:"id"`       // id
+	AuthorID      int64     `gorm:"column:author_id;NOT NULL" json:"author_id"`           // 作者id
+	Description   string    `gorm:"column:description" json:"description"`                // 描述
+	PlayUrl       string    `gorm:"column:play_url;NOT NULL" json:"play_url"`             // 播放地址
+	CoverUrl      string    `gorm:"column:cover_url;NOT NULL" json:"cover_url"`           // 封面地址
+	Title         string    `gorm:"column:title;NOT NULL" json:"title"`                   // 标题
+	favoriteCount int       `gorm:"column:favorite_count;NOT NULL" json:"favorite_count"` // 点赞数
+	PlayCounts    int       `gorm:"column:play_counts;NOT NULL" json:"play_counts"`       // 播放次数
+	CommentCount  int       `gorm:"column:comment_count;NOT NULL" json:"comment_count"`   // 评论次数
+	CreateTime    time.Time `gorm:"column:create_time;default:CURRENT_TIMESTAMP;NOT NULL" json:"create_time"`
+	UpdateTime    time.Time `gorm:"column:update_time;default:CURRENT_TIMESTAMP;NOT NULL" json:"update_time"`
+	IsDeleted     int       `gorm:"column:is_deleted;default:0;NOT NULL" json:"is_deleted"` // (0-, 1-)
 }
